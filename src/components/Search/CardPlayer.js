06e121f095b2a721cfa0f385c56ff87315bcad28
Antonio Fabio <antoniofabiopbreyer@gmail.com>
2023-04-14 21:43:35 -0300

    feat: overflow hidden name

--- src/components/Search/CardPlayer.js
+++ src/components/Search/CardPlayer.js
@@ -1,23 +1,24 @@
 import { Card, Typography, styled as styledMui } from "@mui/material";
-
-
-
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 export function CardPlayer({name, id}) {
   return (
     <>
       <StyledCard onClick={()=>console.log(id)}>
-        <Typography>
+        <Typography sx={{overflow:"hidden", fontFamily:"Playfair Display"}}>
           {name}
         </Typography>
+        <PersonAddIcon/>
       </StyledCard>
     </>
   )
 }
 
 const StyledCard = styledMui(Card)({
+  display:"flex",
+  justifyContent:"space-between",
   width:"100%",
   height:40,
   marginTop:10,
-  padding:"8px 5px"
+  padding:"8px 5px",
 })
